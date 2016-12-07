83f1ea79a60b97c0b36747be1fb489d0b8b9cc70
mSheverda <sheverda_maxym@ukr.net>
2016-12-07 13:43:05 -0800

    Configure ServletContext to save data in UTF-8

--- src/main/java/ua/com/models/configuration/AppInitializer.java
+++ src/main/java/ua/com/models/configuration/AppInitializer.java
@@ -1,6 +1,14 @@
 package ua.com.models.configuration;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import ua.com.models.listener.SessionListener;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -17,6 +25,33 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
     @Override
     protected String[] getServletMappings() {
         return new String[] { "/" };
+    }
+
+    /**
+     * Настройка ссесии.
+
+     */
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        // Управление жизненным циклом корневого контекста приложения
+        servletContext.addListener(new SessionListener());
+
+        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+    }
+
+    /**
+     * Включение исключений NoHandlerFound.
+     *
+     */
+    @Override
+    protected DispatcherServlet createDispatcherServlet(WebApplicationContext context) {
+        final DispatcherServlet dispatcherServlet = (DispatcherServlet) super.createDispatcherServlet(context);
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        return dispatcherServlet;
     }
 
 }
